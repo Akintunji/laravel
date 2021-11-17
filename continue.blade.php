8fb11310fdaf5054141410c2417bba54ae0625e3
Akintunji <92767020+Akintunji@users.noreply.github.com>
2021-11-17 07:20:42 +0200

    Add files via upload

--- continue.blade.php
+++ continue.blade.php
@@ -1,69 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+         
+        </h2>
+        
+    </x-slot>
 
-        <style type="text/css">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                  
+                          
 
-**{
-	margin-left:2px;
-	padding-left:10px;
-text-decoration:none;
-font-size: 12px;
-width: 100%;
-}
+    @foreach($Myjmarts as $row)
+   
 
-header{
-    padding-bottom:40px;
-}
-.header{
-border-radius: 9px; 
-margin-bottom:10px;	
-width: 100%;
-max-height:190px; 
-margin-left: -10px;
-position: relative;
-border-bottom:20px;
-padding-bottom:0px;
-overflow: hidden;
-}
-
-
-
-#header h2{
-margin-left: 30px;
-	font-size: 30px;
-}
-
-.showdiv{
-    border-bottom:20px; 
-    color:purple;
-    border-radius: 9px; 
-margin-bottom:10px;	
- background-color:#6b78b4; 
-}
-
-#b{
-    color:purple;
-}
-</style>
-</head>
-<header>
-<div id="header" class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-<div><p><h2>Thejobmart</h2></p></div>
-
-<button id="b"class="pull-left"><a href="Postjobf">POST JOB</a></button>(RECRUITERS) </button>
-
-</div>
-</div>
-</header>
-<body>     
-                
-
-<div class="showdiv">
-@foreach ($Myjmarts as $row)
-<p align='center'>Organization:{{ $row['firstname']}}</p>
+    <p align='center'>Organization:{{ $row['firstname']}}</p>
 <p align='center'>Job By:{{ $row['firstname']}}</p>
 <p align='center'>Time:{{ $row['lastname']}}</p>
 <p align='center'>Location:{{ $row['location']}}</p>
@@ -74,15 +27,11 @@ margin-bottom:10px;
 <p align='center'><label id='label'>Salary:</label>{{$row['location ']}}</p>
 <p align='center'><label id='label'>Other Informations:</label>{{$row['location ']}}</p>
 
+
+
 @endforeach
-</div>
-
-
-<span> {{$Myjmarts->link()}   </span>
-
-   
-   
-
-</body>
-</html>
-
+ 
+            </div>
+        </div>
+    </div>
+</x-app-layout>
